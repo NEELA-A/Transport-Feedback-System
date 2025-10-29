fb10a6550fa077797919d93e42f5cc545752f346
NEELA-A <220701184@rajalakshmi.edu.in>
2025-10-30 00:41:00 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -11,36 +11,31 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
-resource "azurerm_service_plan" "app_plan" {
-  name                = var.app_service_plan_name
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_service_plan" "plan" {
+  name                = var.app_service_plan
   location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
-resource "azurerm_linux_web_app" "app" {
-  name                = var.app_service_name
+resource "azurerm_linux_web_app" "webapp" {
+  name                = var.webapp_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  service_plan_id     = azurerm_service_plan.app_plan.id
+  service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
     application_stack {
-      docker_image     = "${azurerm_container_registry.acr.login_server}/${var.docker_repo_name}"
-      docker_image_tag = var.docker_image_tag
+      docker_image     = "${var.acr_name}.azurecr.io/feedback-backend-app-na"
+      docker_image_tag = "v5"
     }
   }
 
-  identity {
-    type = "SystemAssigned"
-  }
-
   app_settings = {
-    WEBSITES_PORT             = "3000"
-    DOCKER_REGISTRY_SERVER_URL = "https://${azurerm_container_registry.acr.login_server}"
-    DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.acr.admin_username
-    DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.acr.admin_password
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+    DOCKER_REGISTRY_SERVER_URL          = "https://${var.acr_name}.azurecr.io"
+    DOCKER_REGISTRY_SERVER_USERNAME     = azurerm_container_registry.acr.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD     = azurerm_container_registry.acr.admin_password
   }
 }
-
