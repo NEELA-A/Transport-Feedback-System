a35b8c778a848dc4e1d5c82acc825a2135e25da3
NEELA-A <220701184@rajalakshmi.edu.in>
2025-10-30 00:40:43 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,40 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "FeedbackRG"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "acr_name" {
+  type    = string
+  default = "feedbackacr123"
+}
+
+variable "app_service_plan" {
+  type    = string
+  default = "feedback-app-plan"
+}
+
+variable "webapp_name" {
+  type    = string
+  default = "feedback-backend-app-na"
+}
