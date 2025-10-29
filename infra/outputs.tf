febaf1d5afee382041045d3c10622c8c2fcbab7e
NEELA-A <220701184@rajalakshmi.edu.in>
2025-10-30 00:41:13 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,11 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
 
 output "webapp_url" {
-  value = azurerm_linux_web_app.app.default_hostname
+  value = azurerm_linux_web_app.webapp.default_hostname
 }
-
